0a579dde1bf36ca7e8a16234cecaacffd2dbd07b
Marcela Griet <marcelagriet@gmail.com>
2025-11-11 15:02:40 -0300

    Initial Configurations

---
+++ src/main/java/com/miempresa/miaplicacion/TakeHomeChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.miempresa.miaplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHomeChallengeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TakeHomeChallengeApplication.class, args);
+  }
+}

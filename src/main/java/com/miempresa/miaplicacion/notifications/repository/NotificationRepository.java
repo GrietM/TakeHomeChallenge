ac9c814de072200269eb49c8d160e49aaf11f46d
Marcela Griet <marcelagriet@gmail.com>
2025-12-14 14:39:31 -0300

    refactor: Add Users manage in repositories

--- src/main/java/com/miempresa/miaplicacion/notifications/repository/NotificationRepository.java
+++ src/main/java/com/miempresa/miaplicacion/notifications/repository/NotificationRepository.java
@@ -4,7 +4,10 @@ import com.miempresa.miaplicacion.notifications.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
+    List<Notification> findByUser_Id(Long userId);
 
 }
---
+++ src/main/java/com/miempresa/miaplicacion/users/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.miempresa.miaplicacion.users.repository;
+
+import com.miempresa.miaplicacion.users.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
